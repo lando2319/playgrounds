6f5e347e96c13cdd2ce491fc3effc6f12188a620
Mike Land <mikepland@gmail.com>
2015-01-21 14:53:33 -0600

    add yoCrapsPlaygroundActual

---
+++ yoCrapsPlaygroundActual.playground/section-1.swift
@@ -0,0 +1,13 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+var names = ["Bob", "Larry", "Samantha", "Jeffrey", "Xavier"]
+var names9 = ["Bob", "Samantha", "Jeffrey", "Larry", "Xavier"]
+
+
+
+
+
+
+sort(&names, <)
